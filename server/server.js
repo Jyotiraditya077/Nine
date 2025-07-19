385dca77a9db39fd17207facb8a5b014bad69df1
jyotiraditya swain <jyotiradityaswain123@gmail.com>
2025-07-19 13:33:54 +0530

    backend-updated

--- server/server.js
+++ server/server.js
@@ -26,6 +26,8 @@ mongoose
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const isProduction = process.env.NODE_ENV === "production";
+
 app.use(
   cors({
     origin: process.env.CLIENT_URL,
@@ -38,6 +40,9 @@ app.use(
       "Pragma",
     ],
     credentials: true,
+    optionsSuccessStatus: 200,
+    preflightContinue: false,
+    sameSite: isProduction ? "none" : "lax",
   })
 );
 
