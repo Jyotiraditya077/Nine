b37e319cad2a161f221746ae10d328b4566b8218
jyotiraditya swain <jyotiradityaswain123@gmail.com>
2025-07-19 19:14:31 +0530

    frontend-updates

--- client/src/components/ui/Preloader.jsx
+++ client/src/components/ui/Preloader.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import loaderSvg from "@/assets/9.svg";
 
 function Preloader({ type = "svg", className = "" }) {
   return (
     <div className={`fixed inset-0 flex justify-center items-center w-full h-full ${className} z-50`}>
       <img
-        src="/src/assets/9.svg"
+        src={loaderSvg}
         alt="Loading..."
         className="max-w-full max-h-full"
       />
